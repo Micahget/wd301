7c0db19e5fc8928f1d0ab8db6852c80282eb9b2b
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-08-01 18:23:06 +0530

    Tailwind configured successfull and tailwind import issue fixed

--- hello-react/src/TaskCard.jsx
+++ hello-react/src/TaskCard.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable */
+import './index.css'
 import './TaskCard.css'
 
   const TaskCard = (props) => {
-    console.log(props)
+    // console.log(props)
     return (
       <div className="TaskItem">
-        <h2>{props.title}</h2>
+        <h2 className="text-xl font-bold text-red-600">{props.title}</h2>
         <p>completed on: </p>
         <p>Assignee: Miki G</p>
       </div>
