6ca881c73a917c106767878bad3614739b12323f
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-08-07 19:54:12 +0530

    started working on form

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -14,6 +14,7 @@ class TaskForm extends React.Component<TaskFormProps, TaskFormState> {
     return (
       <form>
         <input type="text" />
+        <button type="submit">Add Task</button>
       </form>
     )
   }
