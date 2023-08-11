3aa3bcf126eb092b30dcdc99084876eee3cae5f9
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-08-11 17:45:19 +0530

    Uncontrolled components

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -1,22 +1,27 @@
 /* eslint-disable */
 import React from "react";
 interface Props {}
-interface TaskFormProps {
-  
-}
-interface TaskFormState {
-}
+interface TaskFormProps {}
+interface TaskFormState {}
 class TaskForm extends React.Component<TaskFormProps, TaskFormState> {
   constructor(props: TaskFormProps) {
     super(props);
+    
   }
-  render(){
+
+  inputRef = React.createRef<HTMLInputElement>(); // we use the ref in uncontrolled components. Uncontrolled components are components that don't have a state. We use them when we don't need to keep track of the state of the compononet. But generally we use controlled components. Controlled components are components that have a state. We use them when we need to keep track of the stat. 
+
+  addTask: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    console.log(`Submitted the form with ${this.inputRef.current?.value}`);
+  };
+  render() {
     return (
-      <form>
-        <input type="text" />
-        <button type="submit">Add Task</button>
+      <form onSubmit={this.addTask}>
+        <input type="text" ref={this.inputRef}/>
+        <button type="submit">Add item</button>
       </form>
-    )
+    );
   }
 }
- export default TaskForm;
+export default TaskForm;
