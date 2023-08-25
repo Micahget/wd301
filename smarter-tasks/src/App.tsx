f882aa7b51101b9c8a67ff2e84d6d9ad0a0d785a
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-08-25 19:51:08 +0530

    IMPLEMENT: Always visible components...using 'app shell'....

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,27 +1,36 @@
 /* eslint-disable */
 import "./App.css";
-// import Task from './Task'
-// import TaskList from './TaskList'
-// import TaskForm from './TaskForm';
-import TaskApp from "./TaskApp";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import TaskListPage from "./pages/TaskListPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from './pages/HomePage';
+import TaskListPage from './pages/TaskListPage';
+// import TaskDetailsPage from "./pages/TaskDetailsPage";
+import Layout from "./Layout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/tasks",
-    element: <TaskListPage />,
+    element: (
+      <Layout />
+    ),
+    children: [
+      {
+        path: "/",
+        element: (<HomePage />)
+      },
+      {
+        path: "tasks",
+        element: (<TaskListPage />)
+      }
+    ]
   }
 ]);
+
 const App = () => {
   return (
     <RouterProvider router={router} />
   );
 }
 
-export default App;
+export default App
