8f548d63f5de0c21c507a2b25f6312a3ba9e3acd
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-09-07 23:55:23 +0530

    WORKING WITH API(EXTERNAL) CONFIG: signin and signup with pupilfirst api we created folder for each and then add index and signin/upForm pages for each and then handel the submittions using async/await to convert the asynchronus to synchronus FILES created: Config folder with constant.tsx file to store constants, and .env file in the root directory to for secure constant declaration....stuff like that...and much more

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,62 +1,36 @@
 /* eslint-disable */
-import "./App.css";
 import {
   createBrowserRouter,
-  Navigate,
   RouterProvider,
 } from "react-router-dom";
-import HomePage from './pages/HomePage';
-import TaskListPage from './pages/TaskListPage';
-import TaskDetailsPage from "./pages/TaskDetailsPage";
-import Layout from "./Layout";
-import Signin from "./pages/Signin"
-import ProtectedRoute from "./ProtectedRoute";
 import Notfound from "./pages/Notfound";
+import Signup from './pages/signup';
+import Signin from './pages/signin/index';
+// import the css
+import "./App.css";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Navigate to="/signin" replace />,
-    },
-    {
-      path: "/signin",
-      element: <Signin />,
-    },
-    {
-      element: (
-        <ProtectedRoute>
-          <Layout />
-        </ProtectedRoute>
-      ),
-      children: [
-        {
-          path: "home",
-          element: <HomePage />,
-        },
-        {
-          path: "tasks",
-          element: <TaskListPage />,
-        },
-        {
-          path: "tasks/:id",
-          element: <TaskDetailsPage />,
-        },
-        {
-          path: "*",
-          element: <Navigate to="/notfound" replace />,
-        },
-        { 
-          path: "/notfound",
-          element: <Notfound />,
-        },
-      ],
-    },
-    {
-      path: "tasks",
-      element: <Signin />,
-    }
-   
-  ]);
+  {
+    path: "/",
+    element: <Signup />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/signin", // then we've added the signin route
+    element: <Signin />,
+  },
+  {
+    path: "/notfound",
+    element: <Notfound />,
+  },
+  {
+    path: "*",
+    element: <Notfound />,
+  }
+]);
 
 const App = () => {
   return (
