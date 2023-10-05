f1c6c30b3fb81a99f00ef656198f0b50a2df947f
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-10-05 12:53:35 +0530

    page/project_details/index.tsx and page/projects/projectContainer file has been edited

--- smarter-tasks/src/pages/projects/ProjectContainer.tsx
+++ smarter-tasks/src/pages/projects/ProjectContainer.tsx
@@ -1,13 +1,18 @@
-import React, { useEffect } from "react";
+/* eslint-disable */
+import { useUsersDispatch } from "../../context/members/context";
+import { fetchUsers } from "../../context/members/actions";
 import { useProjectsDispatch } from "../../context/projects/context";
+import { useEffect } from "react";
 import { fetchProjects } from "../../context/projects/actions";
 import { Outlet } from "react-router-dom";
 
 const ProjectContainer = () => {
   const projectDispatch = useProjectsDispatch();
+  const memberDispatch = useUsersDispatch();
   useEffect(() => {
     fetchProjects(projectDispatch);
-  }, [projectDispatch]);
+    fetchUsers(memberDispatch);
+  }, [projectDispatch, memberDispatch]);
   return <Outlet />;
 };
 
